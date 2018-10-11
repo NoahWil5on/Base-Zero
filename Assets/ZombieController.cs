6a78a6c03e90b92d592a23a63f9a6385ad3dc64c
DESKTOP-GEM9RED\tripl <jxj5614@rit.edu>
2018-10-11 17:01:37 -0400

    NavMesh test level
    Working navmesh seeking in Julain's Test Scene

---
+++ Assets/ZombieController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour {
+
+    public GameObject player;
+    public NavMeshAgent zombie;
+
+	// Update is called once per frame
+	void Update () {
+
+        zombie.SetDestination(player.transform.localPosition);
+	}
+}
