65ece2ffee2ef4e2710b43910ffa38492479ce67
DESKTOP-GEM9RED\tripl <jxj5614@rit.edu>
2018-11-01 18:15:26 -0400

    small stuff

---
+++ Assets/Scripts/Julian_QuestStuff/Quest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest : MonoBehaviour
+{
+
+    private string questStatus;
+
+    public string questName;
+
+    public Text uiText;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public string getQuestStatus()
+    {
+        return questStatus;
+    }
+    public void changeQuestStatus(string newQuestStatus)
+    {
+        questStatus = newQuestStatus;
+    }
+}
