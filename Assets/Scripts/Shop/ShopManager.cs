5ad1389898b97508843e99e8ab4f0ee3b68ca5e6
Julian Januszka (RIT Student) <jxj5614@ad.rit.edu>
2018-11-13 15:32:12 -0500

    Shop stuff and navmesh testing

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -9,18 +9,25 @@ public class ShopManager : MonoBehaviour {
     //Main Shop Screen Array
     public GameObject categoryScreen;
 
+    public GameObject statFlavorScreen;
 
     public GameObject assaultRifleScreen;
 
 	// Use this for initialization
 	void Start () {
 
+        statFlavorScreen.SetActive(false);
         categoryScreen.SetActive(true);
         assaultRifleScreen.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(assaultRifleScreen.activeSelf == true)
+        {
+            statFlavorScreen.SetActive(true);
+        }
 		
 	}
 
