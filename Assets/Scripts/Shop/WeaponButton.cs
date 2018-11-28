13e701f1c66a93cb5c6d5332b70a4eef22a7b51e
Julian Januszka (RIT Student) <jxj5614@ad.rit.edu>
2018-11-28 14:43:06 -0500

    Shop work

--- Assets/Scripts/Shop/WeaponButton.cs
+++ Assets/Scripts/Shop/WeaponButton.cs
@@ -17,4 +17,24 @@ public class WeaponButton : MonoBehaviour {
     {
         element.SetActive(true);
     }
+    public void hideElement(GameObject element)
+    {
+        element.SetActive(false);
+    }
+    public void hideAllChildren(GameObject parentOfChildren)
+    {
+        foreach(Transform child in parentOfChildren.transform)
+        {
+            child.gameObject.SetActive(false);
+            hideAllChildren(child.gameObject);
+        }
+    }
+    public void unhideAllChildren(GameObject parentOfChildren)
+    {
+        foreach (Transform child in parentOfChildren.transform)
+        {
+            child.gameObject.SetActive(true);
+            hideAllChildren(child.gameObject);
+        }
+    }
 }
