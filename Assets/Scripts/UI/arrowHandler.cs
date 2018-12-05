37a9eabf7407936aadfa49bb66f601e9879a16f9
DESKTOP-GEM9RED\tripl <jxj5614@rit.edu>
2018-12-05 11:46:34 -0500

    nav arrow

--- Assets/Scripts/UI/arrowHandler.cs
+++ Assets/Scripts/UI/arrowHandler.cs
@@ -7,38 +7,30 @@ public class arrowHandler : MonoBehaviour
 {
 
     public GameObject goTarget;
-    public GameObject player;
+    public GameObject controller;
+    
 
     void Update()
     {
-        PositionArrow();
+        //Vector3 v = goTarget.transform.position;
+        //v.z = 0f;
+
+
+
+        //this.transform.LookAt(transform.position + transform.forward, v - transform.position);
+
+        //Vector3 targetPosLocal = controller.transform.InverseTransformPoint(goTarget.transform.position);
+        //float targetAng = -Mathf.Atan2(targetPosLocal.x, targetPosLocal.y) * Mathf.Rad2Deg - 90;
+
+        //this.transform.eulerAngles = new Vector3(0, 0, targetAng);
+
+        Vector3 tmpVec = controller.transform.InverseTransformPoint(goTarget.transform.position);
+
+        float angtoTar = Mathf.Atan2(tmpVec.x, tmpVec.z) * Mathf.Rad2Deg;
+        angtoTar += 180.0f;
+        this.transform.localEulerAngles = new Vector3(90, angtoTar, 0);
     }
 
-    void PositionArrow()
-    {
-        //renderer.enabled = false;
 
-        Vector3 v3Pos = Camera.main.WorldToViewportPoint(goTarget.transform.position);
-
-        if (v3Pos.z < Camera.main.nearClipPlane)
-            return;  // Object is behind the camera
-
-        if (v3Pos.x >= 0.0f && v3Pos.x <= 1.0f && v3Pos.y >= 0.0f && v3Pos.y <= 1.0f)
-            return; // Object center is visible
-
-        //renderer.enabled = true;
-        v3Pos.x -= 0.5f;  // Translate to use center of viewport
-        v3Pos.y -= 0.5f;
-        v3Pos.z = 0;      // I think I can do this rather than do a 
-                          //   a full projection onto the plane
-
-        float fAngle = Mathf.Atan2(v3Pos.x, v3Pos.y);
-        transform.localEulerAngles = new Vector3(0.0f, 0.0f, -fAngle * Mathf.Rad2Deg);
-
-        v3Pos.x = 0.5f * Mathf.Sin(fAngle) + 0.5f;  // Place on ellipse touching 
-        v3Pos.y = 0.5f * Mathf.Cos(fAngle) + 0.5f;  //   side of viewport
-        v3Pos.z = Camera.main.nearClipPlane + 0.01f;  // Looking from neg to pos Z;
-        transform.position = Camera.main.ViewportToWorldPoint(v3Pos);
-    }
 }
 
